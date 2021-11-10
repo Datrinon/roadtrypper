7b44ef5fb58835c825ca7db89c7ea775ca9960a5
Datrinon <dtrinh089@gmail.com>
2021-11-10 14:16:40 -0800

    Create PageState.js
    PageState will keep track of the loading state of various components. Essentially since the soon-to-be-created overview and this studio component use async tasks to get data.

---
+++ src/js/components/PageState.js
@@ -0,0 +1,12 @@
+/**
+ * Details the page state of the component. Has
+ * three states -- loading, ready, and not found.
+ */
+
+const PAGE_STATE = {
+  LOADING: "loading",
+  READY: "ready",
+  NOT_FOUND: "not_found"
+}
+
+export default PAGE_STATE;
