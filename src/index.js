b3411884973bd1753696082bcd8573a68a74b3cb
Datrinon <dtrinh089@gmail.com>
2021-11-11 09:56:41 -0800

    Add internal styled component for styling DayCard

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./css/reset.css";
 import "./index.css";
 
 import Router from "./js/components/Router";
--- src/js/components/studio/DayCard.js
+++ src/js/components/studio/DayCard.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
+
+let Card = styled.div`
+  border: 1px solid black;
+  width: 30vw;
+  border-radius: 5px;
+  padding: 5px;
+  max-width: 240px;
+  margin: 5px;
+
+  &:hover {
+    border: 1px solid red;
+  }
+`;
 
 function DayCard({ data }) {
   return (
-    <div>
+    <Card>
       <h2>Day {data.order + 1}: {data.title}</h2>
-    </div>
+      <ul>
+        {data.pois.map((point, index) => {
+          return (
+            <li
+              key={data.id + index}
+              className="day-point"
+            >
+              {point.title}
+            </li>
+          )
+        })}
+      </ul>
+    </Card>
   )
 }
 
