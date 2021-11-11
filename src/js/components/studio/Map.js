96ebc1f511b6fcc019c2872503b4622cf0c60119
Datrinon <dtrinh089@gmail.com>
2021-11-11 14:16:59 -0800

    Add example leaflet map *tooltip* into project
    This tooltip can be used for labeling locations and easily styled.

--- src/js/components/studio/Map.js
+++ src/js/components/studio/Map.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import "../../../css/Map.css";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Tooltip } from 'react-leaflet';
 
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import L from "leaflet";
 
 const MapStyled = styled.div`
   height: 300px;
@@ -13,22 +12,43 @@ const MapStyled = styled.div`
 `
 
 function Map() {
+
+  //  Create the Icon
+  const LeafIcon = L.Icon.extend({
+    options: {}
+  });
+
+  const blueIcon = new LeafIcon({
+    iconUrl:
+      "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|abcdef&chf=a,s,ee00FFFF"
+  });
+  const greenIcon = new LeafIcon({
+    iconUrl:
+      "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|2ecc71&chf=a,s,ee00FFFF"
+  });
+
   return (
-    <MapStyled id="map" data-testid="map">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup.
-            <br />
-            Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </MapStyled>
+    <>
+      <input placeholder="Click here to search a place..." />
+      <MapStyled id="map" data-testid="map">
+        <MapContainer center={[37.34051, -121.97365]} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[37.347, -121.97365]} icon={greenIcon}>
+            <Popup>
+              A pretty CSS3 popup.
+              <br />
+              Easily customizable.
+            </Popup>
+            <Tooltip direction="bottom" offset={[0, 20]} opacity={1} permanent={true}>
+              Permanent Tooltip.
+            </Tooltip>
+          </Marker>
+        </MapContainer>
+      </MapStyled>
+    </>
   )
 }
 
