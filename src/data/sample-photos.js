c8d26785fa4aed243eee196975b25b2935430c11
Datrinon <dtrinh089@gmail.com>
2021-11-13 13:27:51 -0800

    Draft out denormalized trip entry
    * will be denormalized after splitting off POIs and photos.

---
+++ src/data/sample-photos.js
@@ -0,0 +1,9 @@
+const photos = [
+  {
+    dayId: 0,
+    PoiId: 0,
+    id: 0,
+    path: "url01.jpg",
+    description: "A picture of the beach (01)."
+  }
+]
---
+++ src/data/sample-pois.js
@@ -0,0 +1,25 @@
+const SAMPLE_POIS = [
+  {
+    tripId: "dans-0",
+    dayId: 0,
+    id: 0,
+    order: 0,
+    title: "Point Arena",
+    coordinates: [37.769796221341544, -122.48632397567849],
+    description: "We began by first visiting the beach.",
+    photos: [
+      {
+        path: "url01.jpg",
+        description: "A picture of the beach (01)."
+      },
+      {
+        path: "url02.jpg",
+        description: "A picture of the beach (02)."
+      },
+      {
+        path: "url03.jpg",
+        description: "A picture of the beach (03)."
+      }
+    ]
+  }
+]
