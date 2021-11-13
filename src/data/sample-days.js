866189f4413dedc737de533122a4416b9c093d7e
Datrinon <dtrinh089@gmail.com>
2021-11-13 13:10:18 -0800

    Convert photos field into an array of objects rather than just paths.

--- src/data/sample-days.js
+++ src/data/sample-days.js
@@ -14,7 +14,20 @@ const SAMPLE_DAYS = [
         title: "Point Arena",
         coordinates: [37.769796221341544, -122.48632397567849],
         description: "We began by first visiting the beach.",
-        photos: ["url01.jpg", "url02.jpg", "url03.jpg"],
+        photos: [
+          {
+            path: "url01.jpg",
+            description: "A picture of the beach (01)."
+          },
+          {
+            path: "url02.jpg",
+            description: "A picture of the beach (02)."
+          },
+          {
+            path: "url03.jpg",
+            description: "A picture of the beach (03)."
+          }
+        ]
       },
       {
         id: 1,
@@ -22,7 +35,20 @@ const SAMPLE_DAYS = [
         title: "Point Bravo",
         coordinates: [38.004782882158985, -122.54414829028939],
         description: "We went to its sequel.",
-        photos: ["url01.jpg", "url02.jpg", "url03.jpg"],
+        photos: [
+          {
+            path: "url01.jpg",
+            description: "A picture of the beach (01)."
+          },
+          {
+            path: "url02.jpg",
+            description: "A picture of the beach (02)."
+          },
+          {
+            path: "url03.jpg",
+            description: "A picture of the beach (03)."
+          }
+        ]
       },
     ],
   },
@@ -39,7 +65,20 @@ const SAMPLE_DAYS = [
         title: "Point Charlie",
         coordinates: [38.44143980971588, -122.72341248007122],
         description: "We went to its sequel, the beach.",
-        photos: ["url01.jpg", "url02.jpg", "url03.jpg"],
+        photos: [
+          {
+            path: "url01.jpg",
+            description: "A picture of the beach (01)."
+          },
+          {
+            path: "url02.jpg",
+            description: "A picture of the beach (02)."
+          },
+          {
+            path: "url03.jpg",
+            description: "A picture of the beach (03)."
+          }
+        ]
       },
       {
         id: 1,
@@ -47,7 +86,20 @@ const SAMPLE_DAYS = [
         title: "Point Delta",
         coordinates: [39.20241934394004, -123.77596475597744],
         description: "We went to its sequel.",
-        photos: ["url01.jpg", "url02.jpg", "url03.jpg"],
+        photos: [
+          {
+            path: "url01.jpg",
+            description: "A picture of the beach (01)."
+          },
+          {
+            path: "url02.jpg",
+            description: "A picture of the beach (02)."
+          },
+          {
+            path: "url03.jpg",
+            description: "A picture of the beach (03)."
+          }
+        ]
       },
       {
         id: 2,
@@ -55,7 +107,20 @@ const SAMPLE_DAYS = [
         title: "Point Echo",
         coordinates: [39.446531884721345, -123.80626325845876],
         description: "We went to its sequel.",
-        photos: ["url01.jpg", "url02.jpg", "url03.jpg"],
+        photos: [
+          {
+            path: "url01.jpg",
+            description: "A picture of the beach (01)."
+          },
+          {
+            path: "url02.jpg",
+            description: "A picture of the beach (02)."
+          },
+          {
+            path: "url03.jpg",
+            description: "A picture of the beach (03)."
+          }
+        ]
       },
     ],
   },
@@ -72,7 +137,20 @@ const SAMPLE_DAYS = [
         title: "Point Foxtrot",
         coordinates: [40.800864341737125, -124.17428934474253],
         description: "We went to Echo's sequel.",
-        photos: ["url01.jpg", "url02.jpg", "url03.jpg"],
+        photos: [
+          {
+            path: "url01.jpg",
+            description: "A picture of the beach (01)."
+          },
+          {
+            path: "url02.jpg",
+            description: "A picture of the beach (02)."
+          },
+          {
+            path: "url03.jpg",
+            description: "A picture of the beach (03)."
+          }
+        ]
       },
       {
         id: 1,
@@ -80,7 +158,20 @@ const SAMPLE_DAYS = [
         title: "Point Golf",
         coordinates: [40.95013460720063, -124.12410503664906],
         description: "We went to Foxtrot's sequel.",
-        photos: ["url01.jpg", "url02.jpg", "url03.jpg"],
+        photos: [
+          {
+            path: "url01.jpg",
+            description: "A picture of the beach (01)."
+          },
+          {
+            path: "url02.jpg",
+            description: "A picture of the beach (02)."
+          },
+          {
+            path: "url03.jpg",
+            description: "A picture of the beach (03)."
+          }
+        ]
       },
       {
         id: 2,
@@ -88,7 +179,20 @@ const SAMPLE_DAYS = [
         title: "Point Hotel",
         coordinates: [41.75653691963277, -124.17986118946551],
         description: "We went to Golf's sequel.",
-        photos: ["url01.jpg", "url02.jpg", "url03.jpg"],
+        photos: [
+          {
+            path: "url01.jpg",
+            description: "A picture of the beach (01)."
+          },
+          {
+            path: "url02.jpg",
+            description: "A picture of the beach (02)."
+          },
+          {
+            path: "url03.jpg",
+            description: "A picture of the beach (03)."
+          }
+        ]
       },
     ],
   },
