0cf05f7adebcfe518001354e7f8deb72d9e5c8c9
Datrinon <dtrinh089@gmail.com>
2021-11-13 16:23:13 -0800

    Create POIDetails.style.js
    This file will contain styled components used between the detail view component and edit detail view component.

---
+++ src/js/components/studio/POIDetails.style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+
+const s = {
+  Thumbnail: styled.img`
+    width: 64px;
+    height: 64px;
+  `,
+  EditModeInput: styled.input`
+    display: block;
+  `,
+  EditModeTextBox: styled.input`
+    display: block;
+  `
+}
+
+export default s;
