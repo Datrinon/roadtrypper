1ce7009c80138867ab8e9078295efb701b9df988
Datrinon <dtrinh089@gmail.com>
2021-11-18 10:20:58 -0800

    Convert Modal State Manager into Custom Hook
    Using this custom hook, we can reuse this modal elsewhere (and even outside this project if we use styled components).
    
    The hook returns the current values, ways to mutate them, and a ref to connect a placed Modal (so that it can sync its visibility outside of that which is managed by the Modal itself).

--- src/js/components/studio/GalleryHeader.js
+++ src/js/components/studio/GalleryHeader.js
@@ -8,48 +8,19 @@ import { TripContext, TripDispatch } from './Studio';
 import DUMMY_NEW_PHOTO from "../../../data/images/url-toAdd.jpg"
 
 import Modal from './Modal';
+import useModal from '../../hooks/useModal';
 
 function GalleryHeader({ setPhotos }) {
 
   const trip = useContext(TripContext);
   const dispatch = useContext(TripDispatch);
 
-  const [modalVisible, setModalVisible] = useState(false);
-  const [modalTitle, setModalTitle] = useState("");
-  const [modalConfirm, setModalConfirm] = useState(null);
-  const [modalDismiss, setModalDismiss] = useState("");
-  const [modalContents, setModalContents] = useState(null);
-  const modalRef = useRef();
-  const modalObserver = useRef();
-  // why useRef?
-  /**
-   * Assignments to the 'modalMutator' variable from inside React Hook useEffect
-   * will be lost after each render. To preserve the value over time, store it
-   * in a useRef Hook and keep the mutable value in the '.current' property.
-   */
-
-  // The purpose of this effect is to keep track of if the modal closes.
-  // this allows us to sync the state variable in charge of modal visibility.
-  useEffect(() => {
-    const config = { attributes: true, childList: false, subtree: true };
-    const callback = () => {
-      const modalStyle = getComputedStyle(modalRef.current);
-      if (modalStyle.display === 'none') {
-        setModalVisible(false);
-      }
-    }
-
-    modalObserver.current = new MutationObserver(callback);
-    modalObserver.current.observe(modalRef.current, config);
-
-    return () => {
-
-      modalObserver.current.disconnect();
-    }
-  }, []);
+  const [modalValues, modalSetter, modalRef] = useModal();
 
   function addPhoto(e) {
     e.preventDefault();
+
+    console.log("You're supposed to add a photo.");
   }
 
   function showAddPhotoModal() {
@@ -57,11 +28,11 @@ function GalleryHeader({ setPhotos }) {
     // For now, we're limited on what we can do since we don't have Firebase
     // integration yet. However, we can work on getting the dispatch function
     // right.
-    setModalVisible(true);
-    setModalTitle("Add a Photo");
-    setModalConfirm({ msg: "Add", callback: addPhoto });
-    setModalDismiss("Cancel");
-    setModalContents(
+    modalSetter.setVisible(true);
+    modalSetter.setTitle("Add a Photo");
+    modalSetter.setConfirm({ msg: "Add", callback: addPhoto })
+    modalSetter.setDismiss("Cancel");
+    modalSetter.setContent(
       <>
         <input type="file" />
         <label htmlFor="photo-description">
@@ -108,13 +79,13 @@ function GalleryHeader({ setPhotos }) {
           Exit Gallery View
         </button>
       </header>
-        <Modal ref={modalRef}
-          visible={modalVisible}
-          title={modalTitle}
-          confirm={modalConfirm}
-          dismissMsg={modalDismiss}
-          content={modalContents}
-        />
+      <Modal ref={modalRef}
+        visible={modalValues.visible}
+        title={modalValues.title}
+        confirm={modalValues.confirm}
+        dismissMsg={modalValues.dismiss}
+        content={modalValues.content}
+      />
     </>
   )
 }
---
+++ src/js/hooks/useModal.js
@@ -0,0 +1,75 @@
+import React, { useState, useRef, useEffect } from 'react'
+
+/**
+ * A hook for managing the state of a Modal. Call this from the top-level of your
+ * component, then place the Modal component where needs to be used.
+ * 
+ * The parameters are provided in an object as follows:
+ * @param {object} init - Object to initialize the modal with.
+ * @param {boolean} visible - True if visible, false if not.
+ * @param {string} title - The title for the modal.
+ * @param {object} confirm - Optional confirm button to implement in the modal.
+ * Provide as an object in {msg: string, callback: fn}.
+ * @param {element} modalContents - the contents of the modal. 
+ */
+function useModal(init = null) {
+  const [modalVisible, setModalVisible] = useState(init?.visible ?? false);
+  const [modalTitle, setModalTitle] = useState(init?.title ?? "");
+  const [modalConfirm, setModalConfirm] = useState(init?.confirm ?? null);
+  const [modalDismiss, setModalDismiss] = useState(init?.dismiss ?? "");
+  const [modalContents, setModalContents] = useState(init?.contents ?? null);
+
+  const modalRef = useRef();
+  const modalObserver = useRef();
+  // why useRef?
+  /**
+   * Assignments to the 'modalMutator' variable from inside React Hook useEffect
+   * will be lost after each render. To preserve the value over time, store it
+   * in a useRef Hook and keep the mutable value in the '.current' property.
+   */
+
+
+  // The purpose of this effect is to keep track of if the modal closes.
+  // this allows us to sync the state variable in charge of modal visibility.
+  useEffect(() => {
+    const config = { attributes: true, childList: false, subtree: true };
+    const callback = () => {
+      const modalStyle = getComputedStyle(modalRef.current);
+      if (modalStyle.display === 'none') {
+        setModalVisible(false);
+      }
+    }
+
+    modalObserver.current = new MutationObserver(callback);
+    modalObserver.current.observe(modalRef.current, config);
+
+    return () => {
+
+      modalObserver.current.disconnect();
+    }
+  }, []);
+
+    const values = {
+      visible: modalVisible,
+      title: modalTitle,
+      confirm: modalConfirm,
+      dismiss: modalDismiss,
+      content: modalContents
+    };
+
+    const mutators = {
+      setVisible: setModalVisible,
+      setTitle: setModalTitle,
+      setConfirm: setModalConfirm,
+      setDismiss: setModalDismiss,
+      setContent: setModalContents
+    }
+
+    return [
+      values,
+      mutators,
+      modalRef
+    ];
+}
+
+export default useModal;
