cae4acc5d8bd0b02ca564ed934681ed7265b32da
Datrinon <dtrinh089@gmail.com>
2021-11-19 15:04:41 -0800

    Add forwardRef to CountingTextArea
    So we can get the value of the input through a ref instead of using a querySelector.

--- src/js/components/studio/CountingTextArea.js
+++ src/js/components/studio/CountingTextArea.js
@@ -2,34 +2,44 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types';
 
 
-function CountingTextArea({textAreaId, labelText, limit, startText="", classNames}, ref) {
+const CountingTextArea = React.forwardRef(
+  ({
+    textAreaId,
+    labelText,
+    limit,
+    startText = "",
+    classNames
+  },
+    ref) => {
 
-  const [text, setText] = useState(startText);
-  const [charRemaining, setCharRemaining] = useState(limit);
+    const [text, setText] = useState(startText);
+    const [charRemaining, setCharRemaining] = useState(limit);
 
-  function onTextAreaChange(e) {
-    setText(e.target.value);
+    function onTextAreaChange(e) {
+      setText(e.target.value);
 
-    setCharRemaining(limit - e.target.value.length);
+      setCharRemaining(limit - e.target.value.length);
+    }
+
+
+    return (
+      <div className={[...classNames]}>
+        <label htmlFor={textAreaId}>
+          {labelText}
+          <textarea
+            ref={ref}
+            id={textAreaId}
+            type="text"
+            maxLength={limit}
+            value={text}
+            onChange={onTextAreaChange}
+          />
+          <span>{charRemaining} characters left</span>
+        </label>
+      </div>
+    )
   }
-  
-
-  return (
-    <div className={[...classNames]} ref={ref}>
-      <label htmlFor={textAreaId}>
-        {labelText}
-        <textarea
-          id={textAreaId}
-          type="text"
-          maxLength={limit}
-          value={text}
-          onChange={onTextAreaChange}
-           />
-        <span>{charRemaining} characters left</span>
-      </label>
-    </div>
-  )
-}
+);
 
 export default CountingTextArea
 
