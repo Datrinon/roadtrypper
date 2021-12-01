dc90039caaa095f832ea8911ae4466be910e7c8a
Datrinon <dtrinh089@gmail.com>
2021-12-01 10:37:30 -0800

    Add AddPoiSuccess, a component for showing successful adds.
    This is a private component inside of Add POI.

--- src/js/components/studio/AddPOI.js
+++ src/js/components/studio/AddPOI.js
@@ -301,6 +301,33 @@ function NewPoiForm({ day }) {
 }
 
 function AddPoi({ sidebarSetter }) {
+function AddPoiSuccess({ lastAddedPoi }) {
+  const sidebarSetter = useContext(SidebarSetter);
+  const trip = useContext(TripContext);
+
+  function displayForm() {
+    sidebarSetter.setContent(<NewPoiForm />);
+  }
+
+  function displayPoi() {
+    const poi = trip.pois.find(poi => poi.dayId === lastAddedPoi.dayId
+      && poi.order === lastAddedPoi.order);
+
+    sidebarSetter.setContent(<PoiDetails activePin={poi} />);
+  }
+
+  return (
+    <div>
+      <h1>Success! New POI added.</h1>
+      <button onClick={displayForm}>Add Another POI</button>
+      <button onClick={displayPoi}>View Added POI</button>
+    </div>
+  )
+}
+
+function AddPoi() {
+  const sidebarSetter = useContext(SidebarSetter);
+
   function showAddPoi() {
     sidebarSetter.setContent(<NewPoiForm />);
     sidebarSetter.setVisible(true);
