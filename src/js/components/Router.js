2c0a8263e8de2b09e41efb1a597c564e702498c3
Datrinon <dtrinh089@gmail.com>
2021-12-08 11:13:46 -0800

    Add Overview Route, temporarily disable Studio
    Since for studio, it should be an offshoot of the overview component.

--- src/js/components/Router.js
+++ src/js/components/Router.js
@@ -2,18 +2,17 @@
 import React, { useState, useEffect } from 'react';
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 
-
-
 // components
-import Studio from "./studio/Studio"
+import Studio from "./studio/Studio";
+import Overview from "./overview/Overview";
 import Login from './auth/login';
 import SignUp from './auth/signup';
 
 export const UserContext = React.createContext(null);
 
+
 function Router() {
   const [userInfo, setUserInfo] = useState(null);
-
 
   return (
     <HashRouter>
@@ -24,15 +23,18 @@ function Router() {
             {userInfo ? <Redirect to="/studio/" /> : <Redirect to="/signup/login" />}
           </Route>
           {/* Protected Content */}
-          <Route exact path="/studio">
+          {/* <Route exact path="/studio">
             {userInfo ? <Studio /> : <Redirect to="/signup/login" />}
+          </Route> */}
+          <Route exact path="/overview">
+            {userInfo ? <Overview /> : <Redirect to="/signup/login" />}
           </Route>
           {/* Auth pages; redirect if the user is already signed in. */}
           <Route exact path="/signup/login">
-            {userInfo ? <Redirect to="/studio/" /> : <Login setUserInfo={setUserInfo}/>}
+            {userInfo ? <Redirect to="/overview/" /> : <Login setUserInfo={setUserInfo}/>}
           </Route>
           <Route exact path="/signup/" component={SignUp}>
-            {userInfo ? <Redirect to="/studio/" /> : <SignUp />}
+            {userInfo ? <Redirect to="/overview/" /> : <SignUp />}
           </Route>
         </Switch>
       </UserContext.Provider>
