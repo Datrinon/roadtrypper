f50f4fb5441c3ccf8612940b33c73065b2e4607a
Datrinon <dtrinh089@gmail.com>
2021-12-09 05:43:27 -0800

    Add duplicate feature to sample data load to help in creating sort buttons.

--- src/js/database/data.js
+++ src/js/database/data.js
@@ -2,12 +2,32 @@
 import { MOCK_TRIP_ID } from '../../data/sample-days';
 
 
-async function loadSampleTrip(signal) {
+/**
+ * Load a sample trip
+ * @param {AbortController} signal - Allows for aborting
+ * @param {boolean} duplicate - Whether or not to duplicate the trip,
+ * which is useful for layout purposes. Duplicated trips
+ * get a random name and timestamp.
+ * @returns 
+ */
+async function loadSampleTrip(signal, duplicate=false) {
   let trips = [];
 
   let trip = (await import("../../data/sample-trip.json")).default;
 
+  let alphabet = "abcdefghijklmnop";
+
   trips.push(trip);
+
+  if (duplicate) {
+    for (let i = 0; i < 10; i++) {
+      let tripClone = {...trip};
+      tripClone.title = alphabet[i] + "-trip";
+      tripClone.lastAccessed = Date.now() + 176670000 * i;
+      trips.push(tripClone);
+    }
+  } 
+
 
   if (signal.aborted) {
     return Promise.reject(new Error("The request was cancelled early."));
