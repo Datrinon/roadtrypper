a270706df625e66ee4ee83da2ecbaf7f761a0cfa
Datrinon <dtrinh089@gmail.com>
2021-12-10 16:23:54 -0800

    Add middleware function for showing visual feedback when updating DB
    These functions add listener to prevent the user from leaving the page inadvertently, as well as show a loading symbol.
    
    They're shown when the dispatch function is fired in Studio.

---
+++ src/js/database/middleware.js
@@ -0,0 +1,23 @@
+function unloadWarning() {
+  // browser support: 
+  // https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onbeforeunload
+  let confirmationMessage = "\o/";
+  e.returnValue = confirmationMessage; // Gecko, Trident, Chrome 34-51
+  return confirmationMessage;          // Gecko, WebKit, Chrome <34
+}
+
+function onUpdatingStart() {
+  window.addEventListener("beforeunload", unloadWarning);
+  
+  document.querySelector(".timestamp").classList.add("no-display");
+  document.querySelector(".loading").classList.remove("no-display");
+}
+
+function onUpdatingEnd() {
+  window.removeEventListener("beforeunload", unloadWarning);
+
+  document.querySelector(".timestamp").classList.remove("no-display");
+  document.querySelector(".loading").classList.add("no-display");
+}
+
+export { onUpdatingStart, onUpdatingEnd }
