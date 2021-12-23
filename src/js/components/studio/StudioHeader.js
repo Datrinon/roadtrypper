9da4c8cf2a049b2d3884480d712e7c981437de12
Datrinon <dtrinh089@gmail.com>
2021-12-23 09:58:08 -0800

    Add styling for the Studio Header

--- src/js/components/studio/StudioHeader.js
+++ src/js/components/studio/StudioHeader.js
@@ -3,15 +3,17 @@ import { Link } from 'react-router-dom'
 import AccountIcon from '../shared/AccountIcon'
 import HomeLogo from '../shared/HomeLogo'
 
-import * as s from "../styled/template.style"
+import * as s from "../styled/template.style";
+import * as sH from "./styled/StudioHeader.style.js";
 
 
 function StudioHeader() {
   return (
     <s.Header>
-      <HomeLogo/>
-      <span>Here's your header, bro.</span>
-      <AccountIcon />
+      <sH.StudioHeaderContents>
+        <HomeLogo />
+        <AccountIcon />
+      </sH.StudioHeaderContents>
     </s.Header>
   )
 }
---
+++ src/js/components/studio/styled/StudioHeader.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import * as gS from "../../styled/template.style";
+
+export const StudioHeaderContents = styled.div`
+  max-width: 800px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+`
