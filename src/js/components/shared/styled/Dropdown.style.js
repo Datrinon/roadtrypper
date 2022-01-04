18af6a69fc5f6ffbc166806bb170b679f87eaee3
Datrinon <dtrinh089@gmail.com>
2022-01-04 15:19:30 -0800

    Upgrade styling for account icon logout

--- src/js/components/shared/styled/Dropdown.style.js
+++ src/js/components/shared/styled/Dropdown.style.js
@@ -14,11 +14,25 @@ export const DropdownMenuContainer = styled.div`
     border: 1px solid #bebebe;
     border-radius: 3px;
     position: absolute;
-    top: 100%;
-    left: ${props => props.position ? `${props.position}` : "0px"};
+    top: 135%;
+    right: -15%;
     animation: hjECeF 300ms;
     width: max-content;
     background-color: white;
     box-shadow: 0px 2px 3px 0px #3f3f3f54;
     z-index: 999;
+
+    &::after{
+      position: absolute;
+      content: "";
+      border-bottom: 10px solid #b1b1b1;
+      border-left: 10px solid transparent;
+      border-right: 10px solid transparent;
+      border-top: 10px solid transparent;
+      top: -20px;
+      right: 0%;
+      -webkit-transform: translateX(-50%);
+      -ms-transform: translateX(-50%);
+      transform: translateX(-50%);
+    }
 `
